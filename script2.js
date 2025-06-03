be2e27a41428b14a0a4fdba0eefe529b38cd6a8f
MariaAndretti <mariavitoriaandrettimachado85@gmail.com>
2025-06-03 14:29:51 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+let altura = Number(prompt("Informe a sua altura"))
+let genero = confirm("Seu genero é masculino?")
+let pesoideal
+
+if(genero) {
+    pesoideal = 22 * altura ** 2
+
+} else
+
+    pesoideal = 21 * altura ** 2
+    
+    alert(genero + pesoideal)
